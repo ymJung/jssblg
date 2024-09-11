739ebf388163922b6c4e428d7614515308898be7
ymJung <metalbird0@gmail.com>
2024-09-11 23:01:21 +0900

    init commit

---
+++ src/test/java/com/yh/jssblg/JssblgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yh.jssblg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JssblgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
